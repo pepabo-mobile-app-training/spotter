7cdda1e03317c031156671ae9789c4571551f350
endu <futoshi.endo@pepabo.com>
2017-09-29 11:35:39 +0900

    TimeLineで表示する為のTableViewCellを作成した。

---
+++ spotter/Classes/Views/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  spotter
+//
+//  Created by futoshi.endo on 2017/09/29.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
