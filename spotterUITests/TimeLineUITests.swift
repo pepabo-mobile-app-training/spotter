7d64cfb5e0c8c8fc3cc3a7797c836b73d125ad00
endu <futoshi.endo@pepabo.com>
2017-10-03 19:02:10 +0900

    カスタムテーブルビューセルのテストを追加した。

--- spotterUITests/TimeLineUITests.swift
+++ spotterUITests/TimeLineUITests.swift
@@ -24,15 +24,16 @@ class TimeLineUITests: XCTestCase {
         let app = XCUIApplication()
         app.buttons["タイムライン画面"].tap()
         
-        // プロフィール画面の要素を確認する。
-        let profileElements = ["usernameLabel", "useridLavel", "tweetButton", "profileImage"]
-        
-        profileElements.forEach { profileElement in
-            XCTAssert(app.staticTexts["usernameLabel"].exists)
+        let profileElements = ["usernameLabel", "useridLabel"]
+                profileElements.forEach { profileElement in
+            XCTAssert(app.staticTexts[profileElement].exists)
         }
+        XCTAssert(app.images["profileImage"].exists)
+        XCTAssert(app.buttons["tweetButton"].exists)
 
         //カスタムテーブルビューセルの要素を確認する。
-        
+        XCTAssert(app.tables/*@START_MENU_TOKEN@*/.textViews["emoteTextBox"]/*[[".cells.textViews[\"emoteTextBox\"]",".textViews[\"emoteTextBox\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists)
+        XCTAssert(app.tables.images["faceImageView"].exists)
     }
     
 }
