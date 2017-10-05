0b236dc69d85239d13a90ea5ef94d4e6f1da0fdf
Komei Nomura <komei.nomura@pepabo.com>
2017-10-05 15:19:09 +0900

    曲を表示するtableviewを設置

---
+++ spotter/Classes/Controllers/SelectMusicViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SelectMusicViewController.swift
+//  spotter
+//
+//  Created by komei.nomura on 2017/10/05.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class SelectMusicViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var musicTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        musicTableView.register(UINib(nibName: "MusicTableViewCell", bundle: nil), forCellReuseIdentifier: "musicTableCell")
+        musicTableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = musicTableView.dequeueReusableCell(withIdentifier: "musicTableCell", for: indexPath) as! MusicTableViewCell
+        
+        cell.musicImageView.image = UIImage(named: "linkin")
+        cell.musicLabel.text = "hoge\nfuga\npiyo"
+        
+        return cell
+    }
+
+}
