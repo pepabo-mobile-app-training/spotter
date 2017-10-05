46f0fb0dc2d9efce30b36b2a6376dee1ae959f22
endu <futoshi.endo@pepabo.com>
2017-10-05 16:05:23 +0900

    文字に合わせて表示を可変にするように調整した。

--- spotter/Classes/Controllers/TimeLineViewController.swift
+++ spotter/Classes/Controllers/TimeLineViewController.swift
@@ -12,9 +12,20 @@ class TimeLineViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var timelineView: UITableView!
     
+    //  表示するテキスト
+    var DataList:[String] = ["株式会社イーディーエー（英文社名：E.D.A Inc.,株式会社イーディーエー（英文社名：E.D.A Inc.）,株式会社イーディーエー（英文社名：E.D.A Inc.）",
+                             "どうもこんにちは!  \n\n 鶴本です。遠藤です！！！ 遠藤です！！！！,　遠藤です！！！！",
+                             "AutoLayoutで可変UITableViewCellの実装をしてみました!!!!!!!",
+                             "テストテストテストテストテス\nトテストテストテストテストテストテストテストテストテストテストテストテストテストテス\nトテストテストテスト\n\n",
+                             "便利です。"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         timelineView.register(UINib(nibName: "TimelineTableViewCell", bundle: nil), forCellReuseIdentifier: "TimelineTableViewCell")
+        
+        // tableviewcellの表示を可変対応にする。
+        timelineView.estimatedRowHeight = 50
+        timelineView.rowHeight = UITableViewAutomaticDimension
         
     }
 
@@ -23,14 +34,16 @@ class TimeLineViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return DataList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = timelineView.dequeueReusableCell(withIdentifier: "TimelineTableViewCell", for: indexPath) as! TimelineTableViewCell
         
         cell.faceImageView.image = UIImage(named: "komei")
-        cell.tweetText.text = "FF9ありがとうございます! #うれしい「バンザイ ~好きでよかった~」ウルフルズ http://曲が聞けるリンク"
+//        cell.tweetText.text = "FF9ありがとうございます! #うれしい「バンザイ ~好きでよかった~」ウルフルズ http://曲が聞けるリンクFF9ありがとうございます! #うれしい「バンザイ ~好きでよかった~」ウルフルズ "
+        let str = self.DataList[indexPath.row]
+        cell.tweetText.text = str
         
         
         return cell
