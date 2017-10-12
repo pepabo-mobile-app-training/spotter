6d785489fef13f163b626e003d68bd40bcb54fe4
endu <futoshi.endo@pepabo.com>
2017-10-12 17:21:02 +0900

    userIDを追加した。

--- spotter/Classes/Models/Users.swift
+++ spotter/Classes/Models/Users.swift
@@ -10,17 +10,19 @@
 import Foundation
 
 class Users {
+    var userID: Int
     var name: String
     var imgURL: URL
     
-    init(name: String, imgURL: URL) {
+    init(name: String, imgURL: URL, userID: Int) {
         self.name = name
         self.imgURL = imgURL
+        self.userID = userID
     }
     
     static func fetchProfiles(handler: @escaping ((Users) -> Void)) {
-        APIClient.request(endpoint: Endpoint.userProfile) { json in
-            let profiles = Users(name: json["name"].stringValue,  imgURL: (json["img_url"].url)!)
+        APIClient.request(endpoint: Endpoint.userProfile(1)) { json in
+            let profiles = Users(name: json["name"].stringValue, imgURL: (json["img_url"].url)!, userID: json["id"].intValue)
             return handler(profiles)
         }
     }
