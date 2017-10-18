bd98c0dfdd8eab472f288670ff2d1b9ff0d957dc
Komei Nomura <komei.nomura@pepabo.com>
2017-10-18 14:49:40 +0900

    POSTの単体テストを追加

--- spotterTests/testMicropost.swift
+++ spotterTests/testMicropost.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import spotter
+import OHHTTPStubs
 
 class testMicropost: XCTestCase {
     
@@ -25,6 +26,22 @@ class testMicropost: XCTestCase {
          Micropost.fetchMicroposts(userID: 1) { microposts in
             XCTAssertTrue(microposts.count > 1)
             fetchMicropostException?.fulfill()
+        }
+        self.waitForExpectations(timeout: 3, handler: nil)
+    }
+    
+    func testPushMicropost() {
+        stub(condition: isHost("piyorin.xyz") && isPath("/api/microposts/create")) { request in
+            let stubPath = OHPathForFile("successCreateMicropost.json", type(of: self))
+            return fixture(filePath: stubPath!, headers: ["Content-Type":"application/json"])
+        }
+        
+        let userID = 1
+        let content = "fugafuga"
+        let pushMicropostException: XCTestExpectation? =  self.expectation(description: "pushMicropost")
+        Micropost.pushMicropost(userID: userID, content: content) { micropost in
+            XCTAssertEqual(content, micropost.content)
+            pushMicropostException?.fulfill()
         }
         self.waitForExpectations(timeout: 3, handler: nil)
     }
