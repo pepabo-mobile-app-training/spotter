3d821a1c1f373a5609a62f33ae18fdd9bb089d52
Komei Nomura <komei.nomura@pepabo.com>
2017-10-23 12:48:53 +0900

    不要なコメントを削除

--- spotter/Classes/Controllers/LoginViewController.swift
+++ spotter/Classes/Controllers/LoginViewController.swift
@@ -13,12 +13,10 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func pushLoginButton(_ sender: Any) {
