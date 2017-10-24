d49cf29aac80e75852c4e9922d7b4ea9ede68bd5
Komei Nomura <komei.nomura@pepabo.com>
2017-10-24 17:26:43 +0900

    urlにリンクを追加

--- spotter/Classes/Controllers/TimeLineViewController.swift
+++ spotter/Classes/Controllers/TimeLineViewController.swift
@@ -47,7 +47,18 @@ class TimeLineViewController: UIViewController, UITableViewDelegate, UITableView
         }
         
         cell.faceImageView.kf.setImage(with: profile_url)
-        cell.tweetText.text = microposts[indexPath.row].content
+//        cell.tweetText.text = microposts[indexPath.row].content
+        
+        cell.tweetText.isUserInteractionEnabled = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
+        cell.tweetText.addGestureRecognizer(tapGestureRecognizer)
+        
+        let tweetText = microposts[indexPath.row].content
+        let urlText = getMatchStrings(targetString: tweetText, pattern: "(http://|https://){1}[\\w\\.\\-/:]+")[0]
+        let urlRange = (tweetText as NSString).range(of: urlText)
+        let attributedString = NSMutableAttributedString(string: tweetText)
+        attributedString.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.blue, range: urlRange)
+        cell.tweetText.attributedText = attributedString
         return cell
     }
     
@@ -78,5 +89,51 @@ class TimeLineViewController: UIViewController, UITableViewDelegate, UITableView
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             sender.endRefreshing()
         }
+    }
+    
+    @objc func tapGesture(gestureRecognizer: UITapGestureRecognizer) {
+        print("Url tapped")
+//        guard let text = helloLabel.text else { return }
+//        let touchPoint = gestureRecognizer.location(in: helloLabel)
+//        let textStorage = NSTextStorage(attributedString: NSAttributedString(string: linkText))
+//        let layoutManager = NSLayoutManager()
+//        textStorage.addLayoutManager(layoutManager)
+//        let textContainer = NSTextContainer(size: helloLabel.frame.size)
+//        layoutManager.addTextContainer(textContainer)
+//        textContainer.lineFragmentPadding = 0
+//        let toRange = (text as NSString).range(of: linkText)
+//        let glyphRange = layoutManager.glyphRange(forCharacterRange: toRange, actualCharacterRange: nil)
+//        let glyphRect = layoutManager.boundingRect(forGlyphRange: glyphRange, in: textContainer)
+//        if glyphRect.contains(touchPoint) {
+//            print("Tapped")
+//        }
+    }
+    
+    // 正規表現にマッチした文字列を格納した配列を返す
+    func getMatchStrings(targetString: String, pattern: String) -> [String] {
+        var matchStrings:[String] = []
+        
+        do {
+            
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let targetStringRange = NSRange(location: 0, length: (targetString as NSString).length)
+            
+            let matches = regex.matches(in: targetString, options: [], range: targetStringRange)
+            
+            for match in matches {
+                
+                // rangeAtIndexに0を渡すとマッチ全体が、1以降を渡すと括弧でグループにした部分マッチが返される
+                let range = match.range(at: 0)
+                let result = (targetString as NSString).substring(with: range)
+                
+                matchStrings.append(result)
+            }
+            
+            return matchStrings
+            
+        } catch {
+            print("error: getMatchStrings")
+        }
+        return []
     }
 }
